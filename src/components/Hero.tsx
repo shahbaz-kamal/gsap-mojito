84daef5c8157e8fd407ba1620d37ab121bfcdfcd
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-08-13 23:43:43 +0600

    added basic animation to navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,52 @@
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars,words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
+  }, []);
+  return (
+    <div id="hero" className="noisy">
+      <h1 className="title">MOJITO</h1>
+      <img
+        src="/images/hero-left-leaf.png"
+        alt="left-leaf"
+        className="left-leaf"
+      />
+      <img
+        src="/images/hero-right-leaf.png"
+        alt="right-leaf"
+        className="right-leaf"
+      />
+      <div className="body">
+        <div className="content">
+          <div className="space-y-5 hidden md:block">
+            <p>Cool. Crisp. Classic</p>
+            <p className="subtitle">
+              Sip the spirit <br /> of Summer
+            </p>
+          </div>
+          <div className="view-cocktail">
+            <p className="subtitle">
+              Every cocktail on our menu is a blend of premium Ingredients,
+              creative flair, and timeless recipes — designed to delight your
+              senses.
+            </p>
+            <a href="#cocktails">View Cocktails</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
