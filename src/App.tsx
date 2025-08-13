c62d9798c2ee620519331c974efaa002d6d0993c
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-08-13 23:21:01 +0600

    solved hero section showing type error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 // scrollTriger will alow us to animate while scrolling and split text will help us to break text into individual word or characters for detailing
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
-  return <main><Navbar></Navbar></main>;
+  return (
+    <main>
+      <Navbar></Navbar>
+      <section>
+        <Hero></Hero>
+      </section>
+    </main>
+  );
 };
 
 export default App;
-
