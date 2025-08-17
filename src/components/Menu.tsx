9afaf2efd1d95abc9098ca2a0653e06c123dc9f3
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-08-17 21:21:01 +0600

    complete menu section

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { allCocktails } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Menu = () => {
+  const contentRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useGSAP(() => {
+    gsap.fromTo("#title", { opacity: 0 }, { opacity: 1, duration: 1 });
+    gsap.fromTo(
+      ".cocktail img",
+      { opacity: 0, xPercent: "-100" },
+      { xPercent: 0, opacity: 1, ease: "power1.inOut" }
+    );
+    gsap.fromTo('.details h2',{yPercent:100,opacity:0},{yPercent:0,opacity:100,ease:'power1.inOut'})
+    gsap.fromTo('.details p',{yPercent:100,opacity:0},{yPercent:0,opacity:100,ease:'power1.inOut'})
+  }, [currentIndex]);
+
+  const totalCocktails = allCocktails.length;
+  const goToSlides = (index: number) => {
+    const newIndex = (index + totalCocktails) % totalCocktails;
+    setCurrentIndex(newIndex);
+  };
+
+  const getCocktailAt = (indexOfSet: number) => {
+    return allCocktails[
+      (currentIndex + indexOfSet + totalCocktails) % totalCocktails
+    ];
+  };
+  const curentCocktail = getCocktailAt(0);
+  const prevCocktail = getCocktailAt(-1);
+  const nextCocktail = getCocktailAt(1);
+
   return (
     <section id="menu" aria-labelledby="menu-heading">
       <img
@@ -25,7 +55,7 @@ const Menu = () => {
           return (
             <button
               onClick={() => {
-                setCurrentIndex(index);
+                goToSlides(index);
               }}
               key={cocktail.id}
               className={`${
@@ -39,6 +69,45 @@ const Menu = () => {
           );
         })}
       </nav>
+      <div className="content">
+        <div className="arrows">
+          <button
+            className="text-left"
+            onClick={() => goToSlides(currentIndex - 1)}
+          >
+            <span>{prevCocktail.name}</span>
+            <img
+              src="/images/right-arrow.png"
+              alt="right-arrow"
+              area-hidden="true"
+            />
+          </button>
+          <button
+            className="text-left"
+            onClick={() => goToSlides(currentIndex + 1)}
+          >
+            <span>{nextCocktail.name}</span>
+            <img
+              src="/images/left-arrow.png"
+              alt="left-arrow"
+              area-hidden="true"
+            />
+          </button>
+        </div>
+        <div className="cocktail">
+          <img src={curentCocktail.image} alt="" />
+        </div>
+        <div className="recipe">
+          <div ref={contentRef} className="info">
+            <p>Recioe For</p>
+            <p id="title">{curentCocktail.name}</p>
+          </div>
+          <div className="details">
+            <h2>{curentCocktail.title}</h2>
+            <p>{curentCocktail.description}</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
